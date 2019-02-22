e4dbea3ef8d447ec9ade2baa9a73d7d1853830dc
brij <brijgopalbharadwaj1999@gmail.com>
2019-02-22 22:14:06 +0530

    route initialization

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const 
     express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    loginRoute = require('./routes/login'),
+    inputRoute = require('./routes/input'),
+    resultRoute= require('./routes/result');
 
 const app = express();
 const port = process.env.PORT || 3000;
 // =============================================================
+app.use(bodyParser.urlencoded({extended: true}));
+// =============================================================
 app.get("/", (req,res) => res.send("Hi There!!!"));
+app.use("/login", loginRoute);
+app.use("/input", inputRoute);
+app.use("/result", resultRoute);
 // =============================================================
 app.listen(port, () => console.log('Server Online!'));
